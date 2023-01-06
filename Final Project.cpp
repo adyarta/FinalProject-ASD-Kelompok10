556770f93de2e5899e919170684034e203a8079f
SpectreOfSolitude <kakak16903@gmail.com>
2023-01-06 09:56:48 +0700

    Update Fungsi

--- Final Project.cpp
+++ Final Project.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <string>
+#include <stdlib.h>
 using namespace std;
+
+//aplikasi penyimpanan data rawat inap rumah sakit
 
 struct node //node ID antrean
 {
     string nama;
     string ID;
+    string critical;
     node* next;
     node* prev;
 };
@@ -18,11 +23,23 @@ void Display() //display antrean
 
 }
 
-void InsertNormalPatient(string entry) // masukkan antrean baru untuk pasien yang tidak kritis
+void InsertNormalPatient(string entry, int crit) // masukkan antrean baru untuk pasien yang tidak kritis
 //pasien yang tidak kritis akan mengantri dari belakang
 {
     node* temp = new node;
-    temp -> ID = entry;
+    temp -> nama = entry;
+    if (crit >= 3)
+    {
+        temp -> critical = "Kronis";
+    }
+    else if (crit ==2)
+    {
+        temp -> critical = "Darurat";
+    }
+    else if (crit <=1)
+    {
+        temp -> critical = "Rawan";
+    }
 
     if (head == NULL)
     {
@@ -47,18 +64,18 @@ void encryption() //pengenkripsian data asli
 
 int main () //program utama; berisikan main menu
 {
-
+int crit;
 string entri;
 char input;
 MainMenu:
-cout << "=== Aplikasi Administrasi Rumah Sakit ===" << endl;
-cout << "=========================================" << endl;
+cout << "=== Aplikasi Administrasi Rawat Inap Rumah Sakit ===" << endl;
+cout << "====================================================" << endl;
 cout << "Main Menu:"<< endl;
 cout << "1. Daftarkan Antrian pasien" << endl;
 cout << "2. Daftarkan Antrean darurat" << endl;
 cout << "3. Check Out Pasien" << endl;
 cout << "0. Exit" << endl;
-cout << "=========================================" << endl;
+cout << "====================================================" << endl;
 cout << "Entri: "; cin >> input;
 
 switch (input)
@@ -68,7 +85,8 @@ switch (input)
         break;
     case '1':
         cout << "Masukkan data pasien yang ingin didaftarkan: "; cin >> entri;
-        InsertNormalPatient(entri);
+        cout << "Masukkan level kekritisan(1/2/3): "; cin >> crit;
+        InsertNormalPatient(entri, crit);
         break;
     case '2':
         break;
